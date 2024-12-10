f19a7c4473c3b51350f4e722bd040372e6537b57
tabassumnazir <99239595+tabassumnazir@users.noreply.github.com>
2024-12-10 20:29:00 +0800

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,7 +4,7 @@ import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
-import PostReview from "./components/Dealers/PostReview"
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
@@ -14,11 +14,12 @@ function App() {
 
       {/* Route for register */}
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealer/>} />
-      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
     </Routes>
   );
 }
 
 export default App;
+
